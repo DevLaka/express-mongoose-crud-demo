035c57cf2fcd371116d4b5c2d21e310858bd08c1
devlaka <lakshanwarunab@gmail.com>
2022-04-25 17:23:16 +0530

    add create read update functionality

--- index.js
+++ index.js
@@ -2,7 +2,23 @@ const express = require("express");
 const { default: mongoose } = require("mongoose");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 const Course = require("./models/course");
+
+const categories = [
+  {
+    code: "cs",
+    displayName: "Computer Science",
+  },
+  {
+    code: "bs",
+    displayName: "Business Studies",
+  },
+  {
+    code: "eng",
+    displayName: "Engineering",
+  },
+];
 
 mongoose
   .connect("mongodb://localhost:27017/courseApp")
@@ -16,10 +32,46 @@ mongoose
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/courses", async (req, res) => {
   const courses = await Course.find({});
   res.render("courses/index", { courses });
+});
+
+app.get("/courses/new", (req, res) => {
+  res.render("courses/new", { categories });
+});
+
+app.post("/courses", async (req, res) => {
+  const { title, price, category } = req.body;
+  const course = new Course({ title, price, category });
+  await course.save();
+  res.redirect(`/courses/${course._id}`);
+});
+
+app.get("/courses/:id", async (req, res) => {
+  const { id } = req.params;
+  const course = await Course.findById(id);
+  res.render("courses/show", { course });
+});
+
+app.get("/courses/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const course = await Course.findById(id);
+  res.render("courses/edit", { course, categories });
+});
+
+app.put("/courses/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, price, category } = req.body;
+  const course = { title, price, category };
+  const newCourse = await Course.findByIdAndUpdate(id, course, {
+    runValidators: true,
+    new: true,
+  });
+  res.redirect(`/courses/${newCourse._id}`);
 });
 
 app.listen(3000, () => {
